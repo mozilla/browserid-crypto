eaaebbce8a32d309200f6d79c405d525b549dd68
Ben Adida <ben@adida.net>
2011-09-08 09:59:17 -0700

    fixed keygen

--- generate-keypair.js
+++ generate-keypair.js
@@ -6,15 +6,6 @@ var jwk = require("./jwk"),
     fs = require("fs");
 
 var keypair = jwk.KeyPair.generate("RS",64);
-var pk = {
-  alg: "RS",
-  value: keypair.publicKey.serialize()
-};
 
-var sk = {
-  alg: "RS",
-  value: keypair.secretKey.serialize()
-};
-
-fs.writeFileSync("key.publickey", JSON.stringify(pk));
-fs.writeFileSync("key.secretkey", JSON.stringify(sk));
+fs.writeFileSync("key.publickey", keypair.publicKey.serialize());
+fs.writeFileSync("key.secretkey", keypair.secretKey.serialize());
