74b46303ebfbcd22a1015ebbcc52351d9dd21ab2
Ben Adida <ben@adida.net>
2011-10-25 11:23:42 -0700

    updated parameters to FIPS186-3 test vectors, ensured that random generation is done according to FIPS186-3 way

--- algs/ds.js
+++ algs/ds.js
@@ -44,9 +44,14 @@ var HASH_ALGS = {
 };
 
 function doHash(hashAlg, message, modulus) {
-  // FIXME: conversion of hash needs review from FIPS186-3, appendix c.2
-  // NO REALLY FIXME - I'm quite sure the modulus part is not cool 
-  return new libs.BigInteger(HASH_ALGS[hashAlg](message), "16").mod(modulus);
+  // updated for FIPS186-3, section 4.6, and integer/string conversion as per appendix c.2
+  var raw_hash = HASH_ALGS[hashAlg](message);
+
+  // not really taking the minlength of bitlength and hash output, because assuming
+  // that the parameters we use match hash-output bitlength.
+
+  // we don't actually need to do modulus here, because of the previous assumption
+  return new libs.BigInteger(raw_hash, "16");
 }
 
 // pad with leading 0s a hex string
@@ -62,22 +67,27 @@ function hex_lpad(str, length) {
 // this means we don't need to specify a parameter generation step.
 var KEYSIZES = {
   // for testing only
+  // 64 and 128 are the same, since DSA is fast anyways
+  // the following are based on the first FIPS186-3 test vectors for 1024/160 SHA-256
+  // under the category A.2.3 Verifiable Canonical Generation of the Generator g
   64: {
-    p: "00f408ae0bcaf454a0c6a74517af870f496a13dd57eb84924da688be3d9d075130faf02f3871c1952f6c37acd7e306f5dc3d9b4c84936e0542b5ce559d06c84bf93420022e56591521a2a98d1f73f89a1a18d91d3c2bf149863616af1dd210a3a3676ce16ac0b37ba0b412cf2a2f9ccf0ffde5d6f45fd3a4f534cf9473cac00613",
-    q: "00998798e396fb4e368b72b4cf4c610baa00fc8341",
-    g: "7eed437fd5087b8c993f8f8c6ad72248737a5cfe3fce06626c4eb316b7d25e4747d49fd652772b726a667ea3ee45b05af8147271838e2fdac039276955af9adb0cb7a227d339dcf9d48cd2da17881dda47d30d5d9b2ca26e75bc0279c794bab94688381bc762de5e33cce504e428cb543662cda93ac740663310ac9b63b0e102",
+    p: "ff600483db6abfc5b45eab78594b3533d550d9f1bf2a992a7a8daa6dc34f8045ad4e6e0c429d334eeeaaefd7e23d4810be00e4cc1492cba325ba81ff2d5a5b305a8d17eb3bf4a06a349d392e00d329744a5179380344e82a18c47933438f891e22aeef812d69c8f75e326cb70ea000c3f776dfdbd604638c2ef717fc26d02e17",
+    q: "e21e04f911d1ed7991008ecaab3bf775984309c3",
+    g: "c52a4a0ff3b7e61fdf1867ce84138369a6154f4afa92966e3c827e25cfa6cf508b90e5de419e1337e07a2e9e2a3cd5dea704d175f8ebf6af397d69e110b96afb17c7a03259329e4829b0d03bbc7896b15b4ade53e130858cc34d96269aa89041f409136c7242a38895c9d5bccad4f389af1d7a4bd1398bd072dffa896233397a",
     hashAlg: "sha256"
   },
   128: {
-    p: "00f408ae0bcaf454a0c6a74517af870f496a13dd57eb84924da688be3d9d075130faf02f3871c1952f6c37acd7e306f5dc3d9b4c84936e0542b5ce559d06c84bf93420022e56591521a2a98d1f73f89a1a18d91d3c2bf149863616af1dd210a3a3676ce16ac0b37ba0b412cf2a2f9ccf0ffde5d6f45fd3a4f534cf9473cac00613",
-    q: "00998798e396fb4e368b72b4cf4c610baa00fc8341",
-    g: "7eed437fd5087b8c993f8f8c6ad72248737a5cfe3fce06626c4eb316b7d25e4747d49fd652772b726a667ea3ee45b05af8147271838e2fdac039276955af9adb0cb7a227d339dcf9d48cd2da17881dda47d30d5d9b2ca26e75bc0279c794bab94688381bc762de5e33cce504e428cb543662cda93ac740663310ac9b63b0e102",
+    p: "ff600483db6abfc5b45eab78594b3533d550d9f1bf2a992a7a8daa6dc34f8045ad4e6e0c429d334eeeaaefd7e23d4810be00e4cc1492cba325ba81ff2d5a5b305a8d17eb3bf4a06a349d392e00d329744a5179380344e82a18c47933438f891e22aeef812d69c8f75e326cb70ea000c3f776dfdbd604638c2ef717fc26d02e17",
+    q: "e21e04f911d1ed7991008ecaab3bf775984309c3",
+    g: "c52a4a0ff3b7e61fdf1867ce84138369a6154f4afa92966e3c827e25cfa6cf508b90e5de419e1337e07a2e9e2a3cd5dea704d175f8ebf6af397d69e110b96afb17c7a03259329e4829b0d03bbc7896b15b4ade53e130858cc34d96269aa89041f409136c7242a38895c9d5bccad4f389af1d7a4bd1398bd072dffa896233397a",
     hashAlg: "sha256"
   },
+  // the following are based on the first FIPS186-3 test vectors for 2048/256 SHA-256
+  // under the category A.2.3 Verifiable Canonical Generation of the Generator g
   256: {
-    p: "815901aa6d3ced6a0bd488c617351e322c8aef8f9f90adf331a2583d8082ac46f74345a1e1cf561facbdf3239bc3f0ee71618b5d016266caafd48439b034a38f6560cd6b671e3a80248b46809ad8de7a4cc7250469611d59dae8d8af5c6d0f9f3665f9857e04e1134dc94b270e93341449ea503617447ecb83b2c01602878c070d080da464c974d9951c35c1a553407345ee31ebc4a29a3488d5a54702a971ee0a1ea4da93fcf64105040893ff4bec23ca11e8cffa279e899a46891137c28e85f5a2fc9c637af6d26f6b5deba3d60580df41c334ea123331f8b0adeb43ea64a037e0c5ac168c47ce421bc9718ba8357099a0221f778599acd917607f3e3024d7",
-    q: "87974deb793421ce3891540d906ac0806b85a2b95adc211a82ef8b659f8d9d25",
-    g: "75ee80f0a161dd0c025ac818db8d52d193a46655fe0ebd3c289a949f42185f58f2f88f825dcdb3e3e98c0598af87599728f4f0719a8f68b133e82eb1bc4e3b6b8a377a5c6b812d656efcde578fdf515ac6ef628f1564ac907745d53bc6213b74f0cc303bbe68f3ab2220dcacd0ceece7aac3a675aaa0604885a1fb1374e6c08f2dcf503e58ac6487be73b8ab2a10fa62a79522cbc777b6321fd346e0d36ee5a7291955117d8bb4284901eb26804bd2286a14af52f5301c489c80dfeafb7ce496af58479a4c6f57f29ec8c9e4f6b88deb06f5d120859d2d4de06e57b0476f8263f7a4a35f67ed21a4a927109fa89a6b7f4976e98e3ddb3cd232c516b1da5cc555",
+    p: "d6c4e5045697756c7a312d02c2289c25d40f9954261f7b5876214b6df109c738b76226b199bb7e33f8fc7ac1dcc316e1e7c78973951bfc6ff2e00cc987cd76fcfb0b8c0096b0b460fffac960ca4136c28f4bfb580de47cf7e7934c3985e3b3d943b77f06ef2af3ac3494fc3c6fc49810a63853862a02bb1c824a01b7fc688e4028527a58ad58c9d512922660db5d505bc263af293bc93bcd6d885a157579d7f52952236dd9d06a4fc3bc2247d21f1a70f5848eb0176513537c983f5a36737f01f82b44546e8e7f0fabc457e3de1d9c5dba96965b10a2a0580b0ad0f88179e10066107fb74314a07e6745863bc797b7002ebec0b000a98eb697414709ac17b401",
+    q: "b1e370f6472c8754ccd75e99666ec8ef1fd748b748bbbc08503d82ce8055ab3b",
+    g: "9a8269ab2e3b733a5242179d8f8ddb17ff93297d9eab00376db211a22b19c854dfa80166df2132cbc51fb224b0904abb22da2c7b7850f782124cb575b116f41ea7c4fc75b1d77525204cd7c23a15999004c23cdeb72359ee74e886a1dde7855ae05fe847447d0a68059002c3819a75dc7dcbb30e39efac36e07e2c404b7ca98b263b25fa314ba93c0625718bd489cea6d04ba4b0b7f156eeb4c56c44b50e4fb5bce9d7ae0d55b379225feb0214a04bed72f33e0664d290e7c840df3e2abb5e48189fa4e90646f1867db289c6560476799f7be8420a6dc01d078de437f280fff2d7ddf1248d56e1a54b933a41629d6c252983c58795105802d30d7bcd819cf6ef",
     hashAlg: "sha256"
   }
 };
@@ -111,8 +121,10 @@ function _getKeySizeFromYBitlength(size) {
 
 function randomNumberMod(q, rng) {
   // do a few more bits than q so we can wrap around with not too much bias
-  // FIXME: is this a secure enough to get a reasonable distribution on x?
-  return new libs.BigInteger(q.bitLength() + 5, rng).mod(q);
+  // wow, turns out this was actually not far off from FIPS186-3, who knew?
+  // FIPS186-3 says to generate 64 more bits than needed into "c", then to do:
+  // result = (c mod (q-1)) + 1
+  return new libs.BigInteger(q.bitLength() + 64, rng).mod(q.subtract(BigInteger.ONE)).add(BigInteger.ONE);
 }
 
 var KeyPair = function() {
@@ -129,6 +141,7 @@ KeyPair.prototype.generate = function(keysize, progressCB, doneCB) {
   this.keysize= keysize;
 
   // FIXME: should we have a more global rng?
+  // FIXME++: more importantly, should we pass this in as a parameter? I think so XXX
   var rng = new libs.SecureRandom();
   
   // DSA key gen: random x modulo q
