4c243e5a5d183349c59266357d0dfc73f5104fb7
Ben Adida <ben@adida.net>
2011-11-03 20:24:02 -0700

    added sha1 to external libs, not using it yet.

--- libs/exports.js
+++ libs/exports.js
@@ -12,6 +12,10 @@ exports.hex2b64 = hex2b64;
 exports.b64tohex = b64tohex;
 exports.int2char = int2char;
 
+// sha1 exports, no hmac for now
+exports.hex_sha1 = hex_sha1;
+exports.b64_sha1 = b64_sha1;
+
 // this is so we can export the faked window and navigator
 // objects and not write the code for base64 twice
 exports.window = window;
