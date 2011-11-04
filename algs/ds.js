0536f897401be42c7ac9cd491927bec014612d64
Ben Adida <ben@adida.net>
2011-11-03 20:51:47 -0700

    switched ds128 to properly use sha1

--- algs/ds.js
+++ algs/ds.js
@@ -39,7 +39,7 @@ var libs = require("../libs/all"),
 var BigInteger = libs.BigInteger;
 
 var HASH_ALGS = {
-  "sha1": null,
+  "sha1": libs.hex_sha1,
   "sha256": function(message) {return libs.sjcl.codec.hex.fromBits(libs.sjcl.hash.sha256.hash(message));}
 };
 
@@ -74,7 +74,7 @@ var KEYSIZES = {
     p: "ff600483db6abfc5b45eab78594b3533d550d9f1bf2a992a7a8daa6dc34f8045ad4e6e0c429d334eeeaaefd7e23d4810be00e4cc1492cba325ba81ff2d5a5b305a8d17eb3bf4a06a349d392e00d329744a5179380344e82a18c47933438f891e22aeef812d69c8f75e326cb70ea000c3f776dfdbd604638c2ef717fc26d02e17",
     q: "e21e04f911d1ed7991008ecaab3bf775984309c3",
     g: "c52a4a0ff3b7e61fdf1867ce84138369a6154f4afa92966e3c827e25cfa6cf508b90e5de419e1337e07a2e9e2a3cd5dea704d175f8ebf6af397d69e110b96afb17c7a03259329e4829b0d03bbc7896b15b4ade53e130858cc34d96269aa89041f409136c7242a38895c9d5bccad4f389af1d7a4bd1398bd072dffa896233397a",
-    hashAlg: "sha256"
+    hashAlg: "sha1"
   },
   // the following are based on the first FIPS186-3 test vectors for 2048/256 SHA-256
   // under the category A.2.3 Verifiable Canonical Generation of the Generator g
