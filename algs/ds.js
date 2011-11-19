de57b1891be2b6f566c7a5c0431d52c1283c1036
Ben Adida <ben@adida.net>
2011-11-18 17:08:27 -0800

    fix for r/s problem

--- algs/ds.js
+++ algs/ds.js
@@ -200,11 +200,16 @@ PublicKey.prototype.verify = function(message, signature) {
 
   // extract r and s
   var hexlength = params.q_bitlength / 4;
+
+  // we pre-pad with 0s because encoding may have gotten rid of some
+  signature = hex_lpad(signature, hexlength * 2);
+
+  // now this should only happen if the signature was longer
   if (signature.length != (hexlength * 2)) {
     console.log("problem with r/s combo: " + signature.length + "/" + hexlength + " - " + signature);
     return false;
   }
-
+  
   var r = new BigInteger(signature.substring(0, hexlength), 16),
       s = new BigInteger(signature.substring(hexlength, hexlength*2), 16);
 
