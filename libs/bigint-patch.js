fc2469cd0dc4bb2cd6e0dcd9cf8e6eaa2b9a5c7b
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-12-18 13:57:22 +0200

    always generate base64*url* unpadded for key serialization

--- libs/bigint-patch.js
+++ libs/bigint-patch.js
@@ -101,3 +101,11 @@ try {
 } catch (x) {
   // oh well, we use normal JS
 }
+
+BigInteger.prototype.toBase64URL = function() {
+  var s = this.toBase64();
+  s = s.split('=')[0]; // Remove any trailing '='s
+  s = s.replace(/\+/g, '-'); // 62nd char of encoding
+  s = s.replace(/\//g, '_'); // 63rd char of encoding
+  return s
+};
